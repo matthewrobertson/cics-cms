6a12fdd64e8d277f5c4d89b03d6f1cb0a6f5aa1a
Matthew Robertson <matthewrobertson03@gmail.com>
2012-04-17 19:25:48 -0700

    HFAUHHH

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ CmsRails::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['mobile_app.js', 'jquery_mobile.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
